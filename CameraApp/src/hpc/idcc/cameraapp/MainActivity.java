9ffc965243249d8756f80d7c37953be838442e61
Vince <vince@cardinalblue.com>
2013-03-10 17:00:07 +0800

    add button for "camera via intent".

--- CameraApp/src/hpc/idcc/cameraapp/MainActivity.java
+++ CameraApp/src/hpc/idcc/cameraapp/MainActivity.java
@@ -1,15 +1,20 @@
 package hpc.idcc.cameraapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button mButtonIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonIntent = (Button) findViewById(R.id.main_button_intent);
     }
 
     @Override
